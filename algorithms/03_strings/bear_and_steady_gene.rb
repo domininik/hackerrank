b70ab0abce69b1aa16e2de8ecd7ba9c1f946af8d
Dominik Fijas <dominik.fijas@getbase.com>
2017-06-11 20:26:53 +0200

    Add optimal solution for bear-and-steady-gene

--- algorithms/03_strings/bear_and_steady_gene.rb
+++ algorithms/03_strings/bear_and_steady_gene.rb
@@ -11,101 +11,70 @@ class GeneReplacerTest < Test::Unit::TestCase
 end
 
 class GeneReplacer
-  LETTERS = %w{ A C T G }
-
   def initialize(string_size, string)
     @string_size = string_size
     @string = string
-    @diffs = Hash.new(0)
-    @global_letter_counts = {}
-    @substring_size = 0
+    @start_index = 0
+    @end_index = 0
+    @result = Float::INFINITY
   end
 
   def call
-    build_diffs
-
-    return 0 if diffs.empty?
-
-    (min_possible_substring_size..string_size).each do |substring_size|
-      start_index_max = string_size - substring_size - 1
-
-      (0..start_index_max).each do |start_index|
-        end_index = start_index + substring_size - 1
-
-        return substring_size if range_valid?(start_index, end_index)
+    while anything_left? do
+      if !ready_to_replace?
+        take_more
+      else
+        update_result
+        take_less
       end
     end
+    result
   end
 
   private
 
-  attr_reader :string_size, :string, :diffs, :global_letter_counts, :substring_size
+  attr_reader :string_size, :string, :start_index, :end_index, :result
 
-  def build_diffs
-    required_substring_size = string_size / 4
-    string_letters = string.chars
-    letter_counts = Hash.new(0)
-    string_letters.each { |letter| letter_counts[letter] += 1 }
+  def anything_left?
+    start_index < string_size && end_index < string_size
+  end
 
-    LETTERS.each do |letter|
-      diff = letter_counts[letter] - required_substring_size
-      diffs[letter] = diff if diff > 0
+  def ready_to_replace?
+    left_chars_frequencies.all? { |char, frequency| frequency <= required_char_frequency }
+  end
+
+  def take_more
+    last_char = string[end_index]
+    left_chars_frequencies[last_char] -= 1
+    @end_index += 1
+  end
+
+  def take_less
+    first_char = string[start_index]
+    left_chars_frequencies[first_char] += 1
+    @start_index += 1
+  end
+
+  def update_result
+    new_result = end_index - start_index
+    @result = [result, new_result].min
+  end
+
+  def left_chars_frequencies
+    unless @left_chars_frequencies
+      @left_chars_frequencies = Hash.new(0)
+      string.chars.each { |char| @left_chars_frequencies[char] += 1 }
     end
+    @left_chars_frequencies
   end
 
-  def key_for(start_index, end_index)
-    "#{start_index}-#{end_index}"
-  end
-
-  def min_possible_substring_size
-    @min_possible_substring_size ||= diffs.values.reduce(:+)
-  end
-
-  def range_valid?(start_index, end_index)
-    key = "#{start_index}-#{end_index}"
-    build_letter_count_for(start_index, end_index) unless global_letter_counts[key]
-
-    letter_counts = global_letter_counts[key]
-    diffs.all? { |letter, diff| letter_counts[letter] >= diff }
-  end
-
-  def build_letter_count_for(start_index, end_index)
-    previous_end_index_key = key_for(start_index, end_index - 1)
-    previous_start_index_key = key_for(start_index - 1, end_index)
-
-    if prev_letter_counts = (global_letter_counts[previous_end_index_key] || global_letter_counts[previous_start_index_key])
-      letter_counts = prev_letter_counts.dup
-
-      if global_letter_counts[previous_end_index_key]
-        last_letter = string[end_index]
-        last_letter_count = letter_counts[last_letter]
-
-        if last_letter_count
-          letter_counts[last_letter] += 1
-        else
-          letter_counts[last_letter] = 1
-        end
-      elsif global_letter_counts[previous_start_index_key]
-        first_letter = string[start_index - 1]
-        first_letter_count = letter_counts[first_letter]
-
-        if first_letter_count
-          letter_counts[first_letter] -= 1
-        else
-          letter_counts[first_letter] = 0
-        end
-      end
-    else
-      slice = string.slice(start_index..end_index)
-      letter_counts = Hash.new(0)
-      slice.chars.each { |letter| letter_counts[letter] += 1 }
-    end
-    global_letter_counts[key_for(start_index, end_index)] = letter_counts
+  def required_char_frequency
+    @required_char_frequency ||= string_size / 4
   end
 end
 
 class GeneReplacerBenchmark
-  SET_SIZES = [100, 200, 500, 1000, 1500, 2000]
+  SET_SIZES = [100, 1_000, 10_000]
 
   TEST_STRING = 'ACAAAAATAAACAAAAACAAAAAAAAAATAAATACAATAAAAAAAAAAAATGAAATACAACAACAAATAAAATAAAAACGACTAAAAAATAAAAAAAAAAAAAAAAAGAGTACTAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAACACAATCAAAATAAACAAAAAAAAAAAAACCAAAATAATCAACAAAAAAAAAAAAAACAAAAACAACAACAAACAAAAAAAAACACAAACAAAAAAAAAAAAAAAACAAAACAAACAAAAAAAAAAAAACAAAAAAACAAAAAAAAAAAAAAAAACAAAAAAAAAAATAAAAAAAAAAAAAAAAAAAAAACAAACAAAAAAAAAAAATACAAAAAGCTATAAAAAAAAAAAAATTAAAAAACAAAAAAAAATAAAAAAAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAAAAAAGAAAAACAAAAAAAAAAAAAAAAACAACCAAAAAACAAAAAAAAACTAAAAAAAAAAAAAAAAAAAAAAAAAAATAACAAAAAACACAAAAAAAAAAAAGAAAGAAAAAAAACACAAAAAAAAACAAACAAAAAAAAAAAAAAAAAAAGAAAACAAAAAAACAAAAAAAACAAAAAAAAAACAAAAATTGGACAAAAAAAAACAAAAAAAAAAAACAAAAAAAGTAAAACAAATAAAAAAACAAAAAAAACAAAAAAAAAAAAAAAAAACAAAAAAGAAACAAAAAACAAAAAAAAATAACAAAACCAAAAAACAAATAAAAAACAAAAAAAATAACACAAAAAAAAAAAGAAACAAAAAAAAAAAAAAAAAAAAAAATTATAAAAAAAAAAAAAAAACAAAAAAAAAAAAAACAAAAAAAAAAGGAAAAAAAAAAAAAAAAAAAAAAAAAAATAACTAAACAAAAAAAAACAAACAAAAAATCAAAAAAAAAAAAGAAAAAAGAATAAGCAACAAAAACACAAAAAAAAAAAAAAAAAAAAAAAACATAAACAATAATAAAAAAAAAACAAAAAAAACAAAAGAACAACAAAAAACAAAACTAAACAAATAAAAAAAAAAAAACAAAAACTACAAAAAAAAAAAGAAAAAAAAAGAAAAAAAAACAAATAAAAGAAAAAAAAAAAAAAAAAAAACACAAAAAAAAAAATAAAAAAAAAAAAAAAAACAAAATAAACAAAAACAAAGAAAAAAACAAACAAAAAAAAAAAACAAAAAACTAAAAACAAAAAAAAAACAAAACACAAAAAAAAAAAAAAATAAAAAAAAAACAAAAAAACAAAAAGGAAAAAAAAAAAAGAACAAAAAAAAAAACAACAGAAAAAAGAAAAGAAAAAAAAAAAAAGACCACAAAATAAAAAAAAACAACAAACAAAAAAAAACAAAACAAAAAAACGAACAAAAAAAACAAAAACAAAAAAAAAAAAAAAAAAAAAAAGGCAAAAACAAAAAAAACAAAACAAAACAAAAAAACAAAAAAAAATTAAGATAAAGAACAAAAAAAGAAGAGAAAAAATTAACAAAAAAAAAAAAATAAAAAATACAAAAAGAAATAAAAAATACAACACACAACAAAAACGAAAAAAAAAAAAAAAACACAAAATAGAAAAAAAAAAAAAACAAAAAAAAAAAAAAGAAAAAAACAAAAAAAAAAAAATAAAAAAAAACGACACAGAAACAAAAAATAACAAAAAAAAAAAAAATAAAAAAAAAACAAAAAAAAAACAAAAAATAAAAAAAAAAACAAACAAAAAAAAAAAAAAAATAAAAAAAAAAAAAGCAAAACATAAACAAGAAAAAAAAAAAAAGTACAAATAACAAAACAAAAAAGACACTAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAGAAAAAAAACCACAAAACAAAAAAATAAAGCAAAAAAAAAAAAAAAAAAAAAAAAAAAATAAATGAAAAAAAAAAGAAAACCAAAAAAATAAAAGA'
 
@@ -123,9 +92,6 @@ end
 
 # GeneReplacerBenchmark.new.call
 #                            user     system      total        real
-# size = 100             0.010000   0.000000   0.010000 (  0.003107)
-# size = 200             0.010000   0.010000   0.020000 (  0.012646)
-# size = 500             0.070000   0.000000   0.070000 (  0.069262)
-# size = 1000            0.240000   0.000000   0.240000 (  0.250937)
-# size = 1500            0.600000   0.010000   0.610000 (  0.607553)
-# size = 2000            1.200000   0.010000   1.210000 (  1.208044)
+# size = 100             0.000000   0.000000   0.000000 (  0.000272)
+# size = 1000            0.000000   0.000000   0.000000 (  0.001736)
+# size = 10000           0.030000   0.000000   0.030000 (  0.031434)
